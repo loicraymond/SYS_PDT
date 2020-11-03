cf4d8fdc0678265356d09ded53d963e0a534e351
Loïc Raymond <69368464+loicraymond@users.noreply.github.com>
2020-11-03 15:24:36 +0100

    Update Reset_WSUS_Client.bat

--- Reset_WSUS_Client.bat
+++ Reset_WSUS_Client.bat
@@ -1,8 +1,24 @@
-net stop wuauserv 
-reg Delete HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate /v PingID /f 
-reg Delete HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate /v AccountDomainSid /f 
-reg Delete HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate /v SusClientId /f  
-reg Delete HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate /v SusClientIDValidation /f 
-net start wuauserv 
-wuauclt.exe /resetauthorization /detectnow 
-pause
+REM
+REM Nom : Reset_WSUS_Client.bat
+REM Date : 03/11/2020
+REM Auteur : Loïc RAYMOND
+REM
+
+
+REM J'arrete les services Windows Update et BITS
+net stop wuauserv /y
+net stop BITS /y
+
+REM Je supprime les clés de registres
+REG Delete HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate /v SusClientId  /f
+REG Delete HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate /v SusClientIdValidation  /f
+
+REM Je lance une mise à jour des stratégies
+gpupdate
+
+
+REM Je demarre le service Windows Update
+net start wuauserv /y
+
+REM Je réinitialise la connectivité avec le serveur WSUS
+wuauclt.exe /resetauthorization /detectnow
