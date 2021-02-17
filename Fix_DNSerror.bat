b1a495ffe863103c3258858522618eafcad93880
Loïc Raymond <69368464+loicraymond@users.noreply.github.com>
2021-02-17 11:26:52 +0100

    Update Fix_DNSerror.bat

--- Fix_DNSerror.bat
+++ Fix_DNSerror.bat
@@ -1,5 +1,8 @@
 @echo off
 
+echo SUPPRESSION PROXY
+netsh winhttp reset proxy
+
 echo SUPPRESSION CLE DE REGISTRE
 reg delete "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters\SearchList" /f
 
